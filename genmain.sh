93b2838737ee0e0cdf13107d4a8dd114889afbe9
Marc Xiao <marc47marc47@gmail.com>
2022-10-30 01:19:26 +0800

    add description of gcc-Wall

--- genmain.sh
+++ genmain.sh
@@ -1,1 +1,8 @@
+#!/bin/sh
+
+# gcc -Wall option flag
+#   Warning all message
+#   gcc -Wall enables all compiler's warning messages. 
+#   This option should always be used, in order to generate better code.
+
 gcc -Wall main.c libmarc.a -o main.exe
